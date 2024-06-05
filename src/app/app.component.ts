2ebafd696142e0a5fd8447439f953016ad90733f
Aliakse Yausiuk <Aliakseyausiuk@gmail.com>
2024-06-05 14:22:04 +0300

    feat: add components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MainPageComponent } from './pages/main-page/main-page.component';
+import { TechSystemComponent } from './pages/tech-system/tech-system.component';
+import { ProductComponent } from './pages/product/product.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+	selector: 'app-root',
+	standalone: true,
+	imports: [
+		RouterOutlet,
+		MainPageComponent,
+		TechSystemComponent,
+		ProductComponent,
+	],
+	templateUrl: './app.component.html',
+	styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'investment-company-angular';
+	title = 'investment-company-angular';
 }
