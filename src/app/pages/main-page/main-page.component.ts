605d35aaf48cca33c9a16d3df2a63536bf6291ab
Aliakse Yausiuk <Aliakseyausiuk@gmail.com>
2024-06-06 14:58:25 +0300

    fix: divided into components

--- src/app/pages/main-page/main-page.component.ts
+++ src/app/pages/main-page/main-page.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../components/header/header.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
-  selector: 'app-main-page',
-  standalone: true,
-  imports: [],
-  templateUrl: './main-page.component.html',
-  styleUrl: './main-page.component.scss'
+	selector: 'app-main-page',
+	standalone: true,
+	imports: [HeaderComponent, FooterComponent],
+	templateUrl: './main-page.component.html',
+	styleUrl: './main-page.component.scss',
 })
-export class MainPageComponent {
-
-}
+export class MainPageComponent {}
