3c8fea64b70920ee17c6052ebb871c212aeb224b
Aliakse Yausiuk <Aliakseyausiuk@gmail.com>
2024-06-06 17:20:11 +0300

    feat: add pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { MainPageComponent } from './pages/main-page/main-page.component';
 import { TechSystemComponent } from './pages/tech-system/tech-system.component';
 import { ProductComponent } from './pages/product/product.component';
+import { AboutComponent } from './pages/about/about.component';
+import { AuthorsComponent } from './pages/authors/authors.component';
+import { ContactsComponent } from './pages/contacts/contacts.component';
+import { NewsComponent } from './pages/news/news.component';
+import { OffersComponent } from './pages/offers/offers.component';
+import { PrinciplesComponent } from './pages/principles/principles.component';
+import { ProgramsComponent } from './pages/programs/programs.component';
+import { SchemeComponent } from './pages/scheme/scheme.component';
 
 @Component({
 	selector: 'app-root',
@@ -12,6 +20,14 @@ import { ProductComponent } from './pages/product/product.component';
 		MainPageComponent,
 		TechSystemComponent,
 		ProductComponent,
+		AboutComponent,
+		AuthorsComponent,
+		ContactsComponent,
+		NewsComponent,
+		OffersComponent,
+		PrinciplesComponent,
+		ProgramsComponent,
+		SchemeComponent,
 	],
 	templateUrl: './app.component.html',
 	styleUrl: './app.component.scss',
